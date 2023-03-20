cd4e34c231f5a0f201dc64a2ffa60f315b66729f
Vamsi-Cloudangles <111474104+Vamsi-Cloudangles@users.noreply.github.com>
2023-03-20 15:53:56 +0530

    Update Feature_Selection.py

--- Feature_Selection.py
+++ Feature_Selection.py
@@ -1,14 +1,11 @@
 from Feature_Engineering import feature_engineering
-from dvc import dvc
 
 def feature_selection():
     dataset = feature_engineering()
     # Drop the unnecesssary columns
     dataset = dataset.drop(["patient_id"], axis =1)    
     print(dataset.head())
-    dataset.to_csv('cleaned.csv')
-    print("DVC process start")
-    dvc()
+    dataset.to_csv('cleaned_dataset.csv")
     return dataset
 
 feature_selection()
